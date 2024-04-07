0a3bae8766d33501fafe168fbb065167a19436bf
Htoo Aung <kyawaung0924@gmail.com>
2024-04-07 17:07:31 -0600

    fixed solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,6 +2,7 @@ const removeFromArray = function(array, ...items) {
     for (let i = 0; i < array.length; i++) {
         if (items.includes(array[i])) {
             array.splice(i,1);
+            i--;
         }
     }
     return array;
