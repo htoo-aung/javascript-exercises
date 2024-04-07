3f1837be755a066476f797a7aedac90bd20ff4f2
Htoo Aung <kyawaung0924@gmail.com>
2024-04-07 17:06:50 -0600

    finished assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    if (typeof(startNum) === "number" && typeof(endNum) === "number") {
+        if (startNum < 0 || endNum < 0) {
+            return "ERROR";
+        }
+        
+        if (startNum < endNum) {
+            for (let i = startNum; i <= endNum; i++) {
+                sum += i;
+            }
+        }
+        else {
+            for (let i = startNum; i >= endNum; i--) {
+                sum += i;
+            }
+        }
 
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
