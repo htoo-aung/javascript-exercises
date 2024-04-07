23ac093a660bb7c177c44da11c5a5a149932489a
Htoo Aung <kyawaung0924@gmail.com>
2024-04-07 16:09:29 -0600

    finished assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    for (let i = 0; i < array.length; i++) {
+        if (items.includes(array[i])) {
+            array.splice(i,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
