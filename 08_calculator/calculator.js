17bbd010a315b20a38391e837192a29434fc7a99
Htoo Aung <kyawaung0924@gmail.com>
2024-05-14 05:20:27 -0600

    Got correct answers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const arrSum = arr.reduce((total, item) => total + item, 0);
+
+  return arrSum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const arrProduct = arr.reduce((total, item) => total * item, 1);
 
+  return arrProduct;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1;
+  
+  if (n === 0) {
+    return 1;
+  }
+  else {
+    for (let i = 1; i <= n; i++) {
+      product = product * i;
+    }
+  }
+
+  return product;
 };
 
 // Do not edit below this line
