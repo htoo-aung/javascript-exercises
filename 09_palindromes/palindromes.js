2d2326e857d200fcaa9658345f3d0d8fb061f17f
Htoo Aung <kyawaung0924@gmail.com>
2024-05-21 11:22:38 -0600

    Completed functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArray = str.toLowerCase().split('');
+    const filteredArr = strArray.filter((character) => {
+        if (character >= 'a' && character <= 'z') {
+            return true;
+        }
+        else if (character >= '0' && character <= '9') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
 
+    const reverseFilteredArr = filteredArr.reverse();
+
+    if (filteredArr === reverseFilteredArr) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
