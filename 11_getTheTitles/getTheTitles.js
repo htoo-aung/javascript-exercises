33f8609271c4d3258fa3a6640d8fc53dfcd5634a
Htoo Aung <kyawaung0924@gmail.com>
2024-05-25 18:53:00 -0600

    Finished assignment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const titles = [];
+    arr.forEach(obj => titles.push(obj.title));
+    
+    return titles;
 };
 
 // Do not edit below this line
